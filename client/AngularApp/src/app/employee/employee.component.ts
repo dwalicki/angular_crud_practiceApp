cc54ed2782a1263b519dbef0503a02f0207e02da
Daniel Walicki <danielrwalicki@gmail.com>
2018-09-11 00:05:51 -0700

    Initial Commit

---
+++ client/AngularApp/src/app/employee/employee.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+import { EmployeeService } from '../shared/employee.serivce';
+import { EmployeeService } from 'src/app/shared/employee.service';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css'],
+  providers: [EmployeeService]
+})
+export class EmployeeComponent implements OnInit {
+
+  constructor(private employeeService: EmployeeService) { }
+
+  ngOnInit() {
+  }
+
+}
